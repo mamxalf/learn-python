395eb36c0c721c2a4022e6f569b7f81db7c4598e
Hammam Firdaus <h.firdaus@aryo.jp>
2021-01-03 08:25:09 +0700

    try requests package

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-from geometry.triangle import count_wide as luas_segitiga
-from geometry.square import count_wide as luas_persegi, info
+import requests
 
-
-def hitung_luas_persegi(sisi):
-    return sisi * sisi
-
-
-# print(hitung_luas_segitiga(3, 5))
-print(luas_segitiga(3, 2))
-# print(hitung_luas_persegi(5))
-print(info('Luas'))
-print(luas_persegi(4))
+req = requests.get('https://google.com')
+status = req.status_code
+print(status)
